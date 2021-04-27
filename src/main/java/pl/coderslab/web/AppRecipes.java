998f247d44e2ce7a16507c6eb4aec75de74f238a
Katarzyna Pacewicz <katarzyna@pacewi.cz>
2021-04-27 12:22:54 +0200

    lista przepisów

---
+++ src/main/java/pl/coderslab/web/AppRecipes.java
@@ -0,0 +1,18 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.RecipeDao;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AppRecipes", value = "/app/recipes/list")
+public class AppRecipes extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RecipeDao recipeDao = new RecipeDao();
+        request.setAttribute("recipes", recipeDao.findAllRecipes());
+        getServletContext().getRequestDispatcher("/app-recipes.jsp").forward(request, response);
+    }
+}
