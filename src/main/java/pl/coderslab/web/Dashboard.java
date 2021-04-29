1f7954a9bf8555b7310030d1d2e8b365935bde87
Mikolajab <mikolajab93@gmail.com>
2021-04-29 13:45:06 +0200

    first_half

---
+++ src/main/java/pl/coderslab/web/Dashboard.java
@@ -0,0 +1,41 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.PlanDao;
+import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Admin;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/dashboard")
+public class Dashboard extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        HttpSession sess = request.getSession();
+//        Admin admin = (Admin) sess.getAttribute("admin");
+        Admin admin = new Admin();
+        admin.setId(1);
+        RecipeDao recipeDao = new RecipeDao();
+        PlanDao planDao = new PlanDao();
+
+        if (admin == null) {
+            getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+        } else {
+            request.setAttribute("countRecipes", recipeDao.getNumberOfRecipe(admin.getId()));
+            request.setAttribute("countPlans", planDao.getNumberOfPlan(admin.getId()));
+            request.setAttribute("lastPlan", planDao.readLastAdded());
+
+
+            getServletContext().getRequestDispatcher("/dashboard.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
+
+    }
+}
+
