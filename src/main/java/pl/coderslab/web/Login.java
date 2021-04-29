8e110910ffe629fa71cf351a9c2b6a936a688e5a
Filip Krzymiński <filipkrzym@gmail.com>
2021-04-29 15:16:49 +0200

    added login function

--- src/main/java/pl/coderslab/web/Login.java
+++ src/main/java/pl/coderslab/web/Login.java
@@ -1,11 +1,16 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "Login", value = "/login")
 public class Login extends HttpServlet {
@@ -17,7 +22,35 @@ public class Login extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<String> warnings = new ArrayList<>();
 
-        getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        if (email.length() < 1){
+            warnings.add("Email nie został podany!");
+        }
+
+        if (password.length() < 1){
+            warnings.add("Hasło nie zostało podane!");
+        }
+
+        if (warnings.size() > 0) {
+            req.setAttribute("warnings", warnings);
+
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        } else {
+            AdminDao adminDao = new AdminDao();
+            Admin admin = adminDao.auth(email, password);
+
+            if (admin != null){
+                req.setAttribute("loginSuccess", true);
+            } else {
+                req.setAttribute("loginSuccess", false);
+            }
+
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+
     }
 }
