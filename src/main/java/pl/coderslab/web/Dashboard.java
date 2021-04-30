622b09321c1e0379916ec59b39f7d7e6ef3b3561
Mikolajab <mikolajab93@gmail.com>
2021-04-30 11:54:41 +0200

    testing

--- src/main/java/pl/coderslab/web/Dashboard.java
+++ src/main/java/pl/coderslab/web/Dashboard.java
@@ -3,11 +3,15 @@ package pl.coderslab.web;
 import pl.coderslab.dao.PlanDao;
 import pl.coderslab.dao.RecipeDao;
 import pl.coderslab.model.Admin;
+import pl.coderslab.model.Plan;
+import pl.coderslab.model.RecipePlanDetails;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet("/dashboard")
 public class Dashboard extends HttpServlet {
@@ -27,7 +31,12 @@ public class Dashboard extends HttpServlet {
             request.setAttribute("countPlans", planDao.getNumberOfPlan(admin.getId()));
             request.setAttribute("lastPlan", planDao.readLastAdded());
 
+            Plan plan = planDao.read(admin.getId());
+            Map<String, List<RecipePlanDetails>> recipePlanDetailsList = planDao.readPlanDetails(admin.getId());
 
+
+            request.setAttribute("plan", plan);
+            request.setAttribute("recipePlanList", recipePlanDetailsList);
 
             getServletContext().getRequestDispatcher("/dashboard.jsp").forward(request, response);
         }
